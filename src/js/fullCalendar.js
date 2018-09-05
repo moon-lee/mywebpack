583a771cbbffaf44f40fb922328a65aafde2a2a8
Moon Lee <lee.moonkyu@gmail.com>
2018-09-06 01:58:38 +1000

    changed

--- src/js/fullCalendar.js
+++ src/js/fullCalendar.js
@@ -3,6 +3,13 @@ import "fullcalendar/dist/fullcalendar.min.css";
 
 (function ($) {
   var localeCode = "en-au";
+  var date = new Date();
+  var d = date.getDate();
+  var m = date.getMonth();
+  var y = date.getFullYear();
+  var started; 
+  var ended;
+  var categoryClass;
 
   $('#calendar').fullCalendar({
     themeSystem: "bootstrap4",
@@ -16,6 +23,57 @@ import "fullcalendar/dist/fullcalendar.min.css";
     navLinks: true, // can click day/week names to navigate views
     editable: true,
     eventLimit: true, // allow "more" link when too many events
+    selectable: true,
+    selectHelper: true,
 
+    select: function (start, end, allDay) {
+      $('#fc_create').click();
+
+      started = start;
+      ended = end;
+
+      $(".antosubmit").on("click", function () {
+        var title = $("#title").val();
+        if (end) {
+          ended = end;
+        }
+
+        categoryClass = $("#event_type").val();
+
+        if (title) {
+          calendar.fullCalendar('renderEvent', {
+            title: title,
+            start: started,
+            end: end,
+            allDay: allDay
+          },
+            true // make the event "stick"
+          );
+        }
+
+        $('#title').val('');
+
+        calendar.fullCalendar('unselect');
+
+        $('.antoclose').click();
+
+        return false;
+      });
+    },
+    eventClick: function (calEvent, jsEvent, view) {
+      $('#fc_edit').click();
+      $('#title2').val(calEvent.title);
+
+      categoryClass = $("#event_type").val();
+
+      $(".antosubmit2").on("click", function () {
+        calEvent.title = $("#title2").val();
+
+        calendar.fullCalendar('updateEvent', calEvent);
+        $('.antoclose2').click();
+      });
+
+      calendar.fullCalendar('unselect');
+    }
   })
 })(jQuery); // End of use strict;
