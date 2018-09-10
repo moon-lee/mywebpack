5af6c094dd6b98d54f9c21c1417347265793c370
Moon Lee <lee.moonkyu@gmail.com>
2018-09-11 09:39:53 +1000

    tidy again

--- src/js/fullCalendar.js
+++ src/js/fullCalendar.js
@@ -90,24 +90,6 @@ function init_fullcalendar() {
 	})
 }
 
-function getMonthDateRange(year, month) {
-	var moment = require('moment');
-
-	// month in moment is 0 based, so 9 is actually october, subtract 1 to compensate
-	// array is 'year', 'month', 'day', etc
-	var startDate = moment([year, month - 1]);
-
-	// Clone the value before .endOf()
-	var endDate = moment(startDate).endOf('month');
-
-	// just for demonstration:
-	console.log(startDate.toDate());
-	console.log(endDate.toDate());
-
-	// make sure to call toDate() for plain JavaScript date type
-	return { start: startDate, end: endDate };
-}
-
 $(document).ready(function () {
 	init_fullcalendar();
 });
