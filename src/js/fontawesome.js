70c24fa4348c00aa78a2139f10f54e3f296ebc8d
Moon Lee <lee.moonkyu@gmail.com>
2018-09-16 21:00:27 +1000

    after new branch and first commit

--- src/js/fontawesome.js
+++ src/js/fontawesome.js
@@ -4,10 +4,10 @@ import { faBars } from "@fortawesome/free-solid-svg-icons/faBars";
 import { faTable } from "@fortawesome/free-solid-svg-icons/faTable";
 import { faTachometerAlt } from "@fortawesome/free-solid-svg-icons/faTachometerAlt";
 import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons/faSignOutAlt";
-import { faChevronRight } from "@fortawesome/free-solid-svg-icons/faChevronRight";
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons/faChevronLeft";
+import { faWeight } from "@fortawesome/free-solid-svg-icons/faWeight";
+import { faMoneyCheckAlt } from "@fortawesome/free-solid-svg-icons/faMoneyCheckAlt";
 import { faGooglePlusSquare } from "@fortawesome/free-brands-svg-icons/faGooglePlusSquare";
 
-library.add(faCloud, faTable, faBars, faTachometerAlt, faSignOutAlt, faGooglePlusSquare, faChevronLeft, faChevronRight);
+library.add(faCloud, faTable, faBars, faTachometerAlt, faSignOutAlt, faGooglePlusSquare, faWeight, faMoneyCheckAlt);
 
 dom.i2svg();
